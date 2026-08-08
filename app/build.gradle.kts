9da624031272e7ce2647d31f8b90a0fcbdd4303f
Shiva Gupta <shiva.gupta17@gmail.com>
2026-08-08 16:12:26 +0530

    chore: bump version to 4.0
    Update version code and name for the next release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.billtracker.qwvxyz"
     minSdk = 24
     targetSdk = 36
-    versionCode = 3
-    versionName = "3.0"
+    versionCode = 4
+    versionName = "4.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
