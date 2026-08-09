4d51900f47539fee36f00ef64087ef5efb988d7c
Shiva Gupta <shiva.gupta17@gmail.com>
2026-08-10 02:23:20 +0530

    build: bump version to 5.0
    Increment versionCode to 5 and versionName to 5.0 for the next release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
     applicationId = "com.aistudio.billtracker.qwvxyz"
     minSdk = 24
     targetSdk = 36
-    versionCode = 4
-    versionName = "4.0"
+    versionCode = 5
+    versionName = "5.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
